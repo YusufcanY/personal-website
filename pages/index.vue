23203beb05f4770bf45b207e10bc5605667960d9
YusufcanY <playders03@gmail.com>
2021-02-03 17:33:34 +0300

    section layout created animation added

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="flex items-center justify-center h-screen overflow-hidden bg-white">
     <div class="flex flex-row flex-wrap w-1/4">
-      <div v-for="section in sections" :key="section.id" class="flex flex-col w-1/2 p-2">
-        <button
-          :class="
-            `p-2 py-20 focus:outline-none bg-black text-white font-semibold text-center text-4xl ${section.box_round} overflow-hidden`
-          "
+      <div v-for="section in sections" :key="section.id" class="flex relative flex-col w-1/2 p-2">
+        <a
+          :class="section.box_round"
+          class="p-2 py-20 focus:outline-none bg-black text-white font-semibold text-center text-4xl z-30 overflow-hidden"
+          @click="handleSection(section.id)"
         >
           {{ section.title }}
-        </button>
+          <div
+            :id="`box-${section.id}`"
+            class="section-box absolute bg-black top-0 right-0 w-full z-10 transition-all duration-500"
+            :class="{ 'big-section': selectedId === section.id, 'left-0 w-full': !generateClass(section.id) }"
+          />
+        </a>
       </div>
     </div>
   </div>
@@ -18,6 +23,7 @@
 export default {
   data() {
     return {
+      selectedId: 0,
       sections: [
         {
           id: 1,
@@ -42,7 +48,20 @@ export default {
       ],
     };
   },
+  methods: {
+    handleSection(sectionId) {
+      this.selectedId = sectionId;
+    },
+    generateClass(sectionId) {
+      return sectionId % 2;
+    },
+  },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.big-section {
+  width: 1000px;
+  height: 100%;
+}
+</style>
