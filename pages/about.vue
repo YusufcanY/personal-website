32b2f8481c299a88239055bc52bbe741d11f9479
YusufcanY <playders03@gmail.com>
2021-03-19 17:43:48 +0300

    responsive improvement

--- pages/about.vue
+++ pages/about.vue
@@ -5,10 +5,10 @@
         <img :src="require('~/assets/icons/arrow.svg')" width="50px" alt="back" class="back-button">
       </nuxt-link>
     </div>
-    <div class="flex items-center relative">
+    <div class="flex items-start md:items-center relative">
       <div class="w-40 h-40 bg-red-200 rounded-full absolute" />
       <p class="text-xl z-30 pl-10">
-        Hello, i am Yusufcan. I created my first website at 12 years old. After that i developed some C# programs in high school. I learned how to develop web fronted and now i striving to build upon what I know about Frontend. I want to be Fullstack Developer some day.
+        Hello, i am Yusufcan. I created my first website at 12 years old. After that i developed some C# programs in high school. I learned how to develop web fronted and now i striving to build upon what I know about Frontend. I want to be Fullstack Developer one day.
       </p>
     </div>
   </div>
