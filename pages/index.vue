c8870ce6eb8e6960f6f519e4fd72f530d150c5d7
YusufcanY <playders03@gmail.com>
2021-03-19 16:29:57 +0300

    paths and alt texts  updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="body-container p-10">
     <div class="flex flex-col md:flex-row flex-1 justify-center items-center md:space-x-16">
-      <div class="menu-container overflow-hidden flex flex-col justify-center items-center relative pb-8">
+      <div class="overflow-hidden flex flex-col justify-center items-center relative pb-8">
         <div class="menu-button flex justify-center items-center rounded-full w-32 h-32 bg-black">
           <nuxt-link to="/code">
-            <img :src="require('~/assets/icons/apple.png')" width="120px" alt="">
+            <img :src="require('~/assets/icons/home/apple.png')" width="120px" alt="code">
           </nuxt-link>
         </div>
         <p class="menu-title md:absolute text-xl font-bold">
@@ -14,7 +14,7 @@
       <div class="menu-container overflow-hidden flex flex-col justify-center items-center relative pb-8">
         <div class="menu-button flex justify-center items-center rounded-full w-32 h-32 bg-black">
           <nuxt-link to="/design">
-            <img :src="require('~/assets/icons/orange.png')" width="120px" alt="">
+            <img :src="require('~/assets/icons/home/orange.png')" width="120px" alt="design">
           </nuxt-link>
         </div>
         <p class="menu-title md:absolute text-xl font-bold">
@@ -24,7 +24,7 @@
       <div class="menu-container overflow-hidden flex flex-col justify-center items-center relative pb-8">
         <div class="menu-button flex justify-center items-center rounded-full w-32 h-32 bg-black">
           <nuxt-link to="/about">
-            <img :src="require('~/assets/icons/pineapple.png')" width="120px" alt="">
+            <img :src="require('~/assets/icons/home/pineapple.png')" width="120px" alt="about">
           </nuxt-link>
         </div>
         <p class="menu-title md:absolute text-xl font-bold">
@@ -34,7 +34,7 @@
       <div class="menu-container overflow-hidden flex flex-col justify-center items-center relative pb-8">
         <div class="menu-button flex justify-center items-center rounded-full w-32 h-32 bg-black">
           <nuxt-link to="/contact">
-            <img :src="require('~/assets/icons/watermelon.png')" width="120px" alt="">
+            <img :src="require('~/assets/icons/home/watermelon.png')" width="120px" alt="contact">
           </nuxt-link>
         </div>
         <p class="menu-title md:absolute text-xl font-bold">
