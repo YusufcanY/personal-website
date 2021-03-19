ff0c2c3be153b5d7bfb1f0b4afacb4e504b74915
YusufcanY <playders03@gmail.com>
2021-03-19 16:30:47 +0300

    contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,53 @@
 <template>
-  <div>sa</div>
+  <div class="body-container p-10">
+    <div class="flex flex-col md:flex-row flex-1 justify-center items-center md:space-x-16">
+      <div class="pb-8">
+        <nuxt-link to="/">
+          <img :src="require('~/assets/icons/arrow.svg')" width="50px" alt="back" class="back-button">
+        </nuxt-link>
+      </div>
+      <div class="menu-container overflow-hidden flex flex-col justify-center items-center relative pb-8">
+        <div class="menu-button flex justify-center items-center rounded-full w-32 h-32 border-2 border-black hover:border-opacity-0 transition-all duration-300">
+          <a href="https://github.com/YusufcanY">
+            <img :src="require('~/assets/icons/contact/github.svg')" width="120px" alt="github">
+          </a>
+        </div>
+        <p class="menu-title md:absolute text-xl font-bold">
+          Github
+        </p>
+      </div>
+      <div class="menu-container overflow-hidden flex flex-col justify-center items-center relative pb-8">
+        <div class="menu-button flex justify-center items-center rounded-full w-32 h-32 border-2 border-black hover:border-opacity-0 transition-all duration-300">
+          <a href="https://www.instagram.com/yusufcan.n">
+            <img :src="require('~/assets/icons/contact/instagram.svg')" width="120px" alt="instagram">
+          </a>
+        </div>
+        <p class="menu-title md:absolute text-xl font-bold">
+          Instagram
+        </p>
+      </div>
+      <div class="menu-container overflow-hidden flex flex-col justify-center items-center relative pb-8">
+        <div class="menu-button flex justify-center items-center rounded-full w-32 h-32 border-2 border-black hover:border-opacity-0 transition-all duration-300">
+          <a href="mailto:ycanny1@gmail.com">
+            <img :src="require('~/assets/icons/contact/mail.svg')" width="120px" alt="mail">
+          </a>
+        </div>
+        <p class="menu-title md:absolute text-xl font-bold">
+          Mail
+        </p>
+      </div>
+      <div class="menu-container overflow-hidden flex flex-col justify-center items-center relative pb-8">
+        <div class="menu-button flex justify-center items-center rounded-full w-32 h-32 border-2 border-black hover:border-opacity-0 transition-all duration-300">
+          <a href="https://www.linkedin.com/in/yusufcan-yilmaz/">
+            <img :src="require('~/assets/icons/contact/linkedin.svg')" width="120px" alt="linkedin">
+          </a>
+        </div>
+        <p class="menu-title md:absolute text-xl font-bold">
+          Linkedin
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +57,17 @@ export default {
 </script>
 
 <style>
-
+.menu-button img{
+  transition: 0.3s cubic-bezier(0.075, 0.82, 0.165, 1);
+}
+.menu-button:hover img{
+  transform: scale(1.4);
+}
+.menu-title{
+  transition: 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);
+  bottom: -18%;
+}
+.menu-button:hover + .menu-title{
+  bottom: 0%;
+}
 </style>
