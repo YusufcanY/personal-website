7f9d1613afee3795651f0c156923d5db5f11ed50
YusufcanY <playders03@gmail.com>
2021-03-25 15:37:15 +0300

    code page improvements

--- pages/code.vue
+++ pages/code.vue
@@ -3,51 +3,62 @@
     <div class="overflow-hidden flex flex-col justify-center items-center relative z-20">
       <div class="menu-button flex justify-center items-center rounded-full w-32 h-32 bg-black">
         <a class="cursor-pointer" @click="toggleFrontMenu">
-          <img :src="require('~/assets/icons/home/apple.png')" width="120px" alt="code">
+          <img :src="require('~/assets/icons/home/apple.png')" width="120px" alt="front">
         </a>
       </div>
     </div>
-    <!-- <transition name="slide-menu"> -->
-    <div class="front-menu">
-      <transition name="slide-menu">
-        <div v-if="frontMenuStat" class="front-menu-item-1 flex justify-center absolute items-center rounded-full w-16 h-16 bg-black">
-          <a class="cursor-pointer" @click="toggleFrontMenu">
-            <img :src="require('~/assets/icons/home/apple.png')" width="120px" alt="code">
-          </a>
-        </div>
-      </transition>
-      <transition name="slide-menu">
-        <div v-if="frontMenuStat" class="front-menu-item-2 flex justify-center absolute items-center rounded-full w-16 h-16 bg-black">
-          <a class="cursor-pointer" @click="toggleFrontMenu">
-            <img :src="require('~/assets/icons/home/apple.png')" width="120px" alt="code">
-          </a>
-        </div>
-      </transition>
-      <transition name="slide-menu">
-        <div v-if="frontMenuStat" class="front-menu-item-3 flex justify-center absolute items-center rounded-full w-16 h-16 bg-black">
-          <a class="cursor-pointer" @click="toggleFrontMenu">
-            <img :src="require('~/assets/icons/home/apple.png')" width="120px" alt="code">
-          </a>
-        </div>
-      </transition>
-      <transition name="slide-menu">
-        <div v-if="frontMenuStat" class="front-menu-item-4 flex justify-center absolute items-center rounded-full w-16 h-16 bg-black">
-          <a class="cursor-pointer" @click="toggleFrontMenu">
-            <img :src="require('~/assets/icons/home/apple.png')" width="120px" alt="code">
-          </a>
-        </div>
-      </transition>
+    <div class="bubble-menu">
+      <bubble-info v-for="item in frontItems" :key="item.id" :index="item.id" :item-name="item.name" :menu-stat="frontMenuStat" />
     </div>
-    <!-- </transition> -->
   </div>
 </template>
 
 <script>
+import BubbleInfo from '~/components/BubbleInfo.vue';
 
 export default {
+  components: { BubbleInfo },
   data() {
     return {
       frontMenuStat: false,
+      frontItems: [
+        {
+          id: '1',
+          name: 'vue',
+        },
+        {
+          id: '2',
+          name: 'react',
+        },
+        {
+          id: '3',
+          name: 'javascript',
+        },
+        {
+          id: '4',
+          name: 'html-5',
+        },
+        {
+          id: '5',
+          name: 'css-3',
+        },
+        {
+          id: '6',
+          name: 'tailwind',
+        },
+        {
+          id: '7',
+          name: 'nuxt',
+        },
+        {
+          id: '8',
+          name: 'php',
+        },
+        {
+          id: '9',
+          name: 'laravel',
+        },
+      ],
     };
   },
   methods: {
@@ -58,39 +69,11 @@ export default {
 };
 </script>
 
-<style>
-.menu-button img{
+<style lang="scss" scoped>
+.menu-button img {
   transition: 0.3s cubic-bezier(0.075, 0.82, 0.165, 1);
 }
-.menu-button:hover img{
+.menu-button:hover img {
   transform: scale(1.4);
-}
-.front-menu-item-1{
-  top: 50%;
-  left: 43%;
-  transform: translate(-50%,-50%);
-}
-.front-menu-item-2{
-  top: 37%;
-  left: 50%;
-  transform: translate(-50%,-50%);
-}
-.front-menu-item-3{
-  top: 50%;
-  left: 57%;
-  transform: translate(-50%,-50%);
-}
-.front-menu-item-4{
-  top: 63%;
-  left: 50%;
-  transform: translate(-50%,-50%);
-}
-.slide-menu-enter-active, .slide-menu-leave-active {
-  transition: .3s cubic-bezier(0.455, 0.03, 0.515, 0.955) ;
-}
-.slide-menu-enter, .slide-menu-leave-to{
-  top: 50%;
-  left: 50%;
-  opacity: 0;
 }
 </style>
