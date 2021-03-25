2a27dd8e52947005de9a97af1b570dd1c03b5464
YusufcanY <playders03@gmail.com>
2021-03-25 18:00:51 +0300

    back button component

---
+++ components/BackButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="absolute top-0 left-0 p-6 md:p-12 z-50">
+    <nuxt-link to="/">
+      <img :src="require('~/assets/icons/arrow.svg')" width="50px" alt="back" class="back-button">
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
