f57f690bd9492887b825169c8ee43e5bf448170a
YusufcanY <playders03@gmail.com>
2021-04-19 06:09:15 +0300

    text updated and minor improvements

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container mx-auto flex flex-col h-screen">
-    <div class="flex justify-center item w-full">
+    <div class="flex justify-center w-full">
       <img :src="require('~/assets/illustration-profile.png')" width="400px" alt="">
     </div>
-    <div class="flex items-start md:items-center relative">
-      <div class="w-20 h-20 md:w-40 md:h-40 bg-red-200 rounded-full absolute" />
+    <div class="flex items-start relative">
+      <div class="w-20 h-20 md:w-32 md:h-32 bg-red-200 rounded-full absolute" />
       <p class="text-xl z-30 px-5 md:pl-10">
-        Hello, i am Yusufcan. I created my first website at 12 years old. After that i developed some C# programs in high school. I learned how to develop web fronted and now i striving to build upon what I know about Frontend. I want to be Fullstack Developer one day.
+        &nbsp;&nbsp; Hello, i am Yusufcan. I am Fronend Developer and thats my website. If you wannna learn about my skills you can check *Code* page or *Design* page. I doing skatebording in my freetimes. Also playing some video games. Sometimes with my office mates (after work :) ). I pretty well with my office mates, after work or in work. I speak English but I can not practice my accent because I live in Turkey. <br><br>&nbsp;&nbsp; About how i discovered web developing, i created my first website at 12 years old. After that i developed some C# programs in high school. I learned how to develop web fronted and now i striving to build upon what I know about Frontend.
       </p>
     </div>
   </div>
