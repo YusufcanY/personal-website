1b9726956ed842c128892b4894664d7af2a64a99
YusufcanY <playders03@gmail.com>
2021-04-21 12:55:07 +0300

    about minor improvements

--- pages/about.vue
+++ pages/about.vue
@@ -6,7 +6,11 @@
     <div class="flex items-start relative">
       <div class="w-20 h-20 md:w-32 md:h-32 bg-red-200 rounded-full absolute" />
       <p class="text-xl z-30 px-5 md:pl-10">
-        &nbsp;&nbsp; Hello, i am Yusufcan. I am Fronend Developer and thats my website. If you wannna learn about my skills you can check *Code* page or *Design* page. I doing skatebording in my freetimes. Also playing some video games. Sometimes with my office mates (after work :) ). I pretty well with my office mates, after work or in work. I speak English but I can not practice my accent because I live in Turkey. <br><br>&nbsp;&nbsp; About how i discovered web developing, i created my first website at 12 years old. After that i developed some C# programs in high school. I learned how to develop web fronted and now i striving to build upon what I know about Frontend.
+        &nbsp;&nbsp; Hello, i am Yusufcan. I am Fronend Developer and thats my website. If you wannna learn about my skills you can check <nuxt-link to="/code">
+          Code
+        </nuxt-link> page or <nuxt-link to="/design">
+          Design
+        </nuxt-link> page. I doing skatebording in my freetimes. Also playing some video games. Sometimes with my office mates. I pretty well with my office mates, after work or in work. I speak English but I can not practice my accent because I live in Turkey. <br><br>&nbsp;&nbsp; About how i discovered web developing, i created my first website at 12 years old. After that i developed some C# programs in high school. I learned how to develop web fronted and now i striving to build upon what I know about Frontend.
       </p>
     </div>
   </div>
@@ -18,6 +22,12 @@ export default {
 };
 </script>
 
-<style>
-
+<style scoped>
+a{
+  font-weight: bold!important;
+  transition: 0.5s ease;
+}
+a:hover{
+  color: red;
+}
 </style>
