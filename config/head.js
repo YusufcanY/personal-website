cfafdb44ec827b6d35f7331bc7058e6726d28f56
YusufcanY <playders03@gmail.com>
2021-04-21 12:54:49 +0300

    favicon update

--- config/head.js
+++ config/head.js
@@ -10,13 +10,14 @@ const head = {
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'yandex-verification', content: 'ab8fd41b5562f8ae' },
     {
       hid: 'description',
       name: 'description',
       content: process.env.npm_package_description || '',
     },
   ],
-  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
 };
 
 export default head;
