132f65cbe68bbab4f2a0bb4e0b3d60084d84c36b
YusufcanY <playders03@gmail.com>
2021-05-17 16:01:11 +0300

    code page design updated

--- pages/code.vue
+++ pages/code.vue
@@ -1,94 +1,62 @@
 <template>
-  <div class="body-container flex flex-col h-screen relative justify-center items-center">
-    <div class="overflow-hidden flex flex-col justify-center items-center relative z-20">
-      <div class="menu-button flex justify-center items-center rounded-full w-32 h-32 bg-black">
-        <a class="cursor-pointer" @click="toggleBubbleMenu">
-          <img :src="require('~/assets/icons/home/apple.png')" width="120px" alt="front">
-        </a>
-      </div>
-    </div>
-    <div class="bubble-menu">
-      <bubble-info
-        v-for="item in bubbleItems"
-        :key="item.id"
-        :index="item.id"
-        :item-name="item.name"
-        :menu-stat="bubbleMenuStat"
-        folder-name="code"
-      />
-    </div>
+  <div class="body-container flex flex-row flex-wrap justify-center items-start py-32">
+    <skill-card v-for="(item, index) in skills" :key="index" :icon="item.name" icon-path="code">
+      <template>
+        <div>
+          {{ item.content }}
+        </div>
+      </template>
+    </skill-card>
   </div>
 </template>
 
 <script>
-import BubbleInfo from '~/components/BubbleInfo.vue';
+import SkillCard from '~/components/SkillCard.vue';
 
 export default {
-  components: { BubbleInfo },
+  components: { SkillCard },
   data() {
     return {
-      bubbleMenuStat: false,
-      bubbleItems: [
+      skills: [
         {
-          id: 1,
-          name: 'vue',
-        },
-        {
-          id: 2,
           name: 'javascript',
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis culpa nesciunt cupiditate maiores!',
         },
         {
-          id: 3,
-          name: 'php',
+          name: 'vue',
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis culpa nesciunt cupiditate maiores!',
         },
         {
-          id: 4,
-          name: 'html-5',
-        },
-        {
-          id: 5,
           name: 'nuxt',
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis culpa nesciunt cupiditate maiores!',
         },
         {
-          id: 6,
           name: 'react',
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis culpa nesciunt cupiditate maiores!',
         },
         {
-          id: 7,
-          name: 'laravel',
+          name: 'html-5',
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis culpa nesciunt cupiditate maiores!',
         },
         {
-          id: 8,
           name: 'css-3',
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis culpa nesciunt cupiditate maiores!',
         },
         {
-          id: 12,
           name: 'tailwind',
+          content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis culpa nesciunt cupiditate maiores!',
         },
       ],
     };
   },
-  methods: {
-    toggleBubbleMenu() {
-      this.bubbleMenuStat = !this.bubbleMenuStat;
-    },
-  },
 };
 </script>
 
-<style lang="scss" scoped>
-.menu-button img {
-  transition: 0.3s cubic-bezier(0.075, 0.82, 0.165, 1);
+<style scoped>
+.body-container > *{
+  margin: 10px;
 }
-.menu-button:hover img {
-  transform: scale(1.4);
-}
-.body-container {
-  height: 100vh;
-}
-@media screen and (max-height: 950px) {
-  .body-container {
-    height: 953px;
-  }
+.border-primary{
+  border: 1px solid rgba(0, 0, 0, 0.125)
 }
 </style>
