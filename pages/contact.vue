2de03a81f3796c48641417b15c1921ca51cfeb1e
YusufcanY <playders03@gmail.com>
2022-01-25 12:38:00 +0300

    Medium button added to contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,9 +2,11 @@
   <div class="p-10">
     <div class="flex flex-col items-center justify-center flex-1 md:flex-row md:h-screen md:space-x-16">
       <div class="relative flex flex-col items-center justify-center pb-8 overflow-hidden menu-container">
-        <div class="flex items-center justify-center w-32 h-32 transition-all duration-300 border-2 border-white rounded-full menu-button hover:border-opacity-0">
+        <div
+          class="flex items-center justify-center w-32 h-32 transition-all duration-300 border-2 border-white rounded-full menu-button hover:border-opacity-0"
+        >
           <a href="https://github.com/YusufcanY">
-            <img :src="require('~/assets/icons/contact/github.svg')" width="120px" alt="github">
+            <img :src="require('~/assets/icons/contact/github.svg')" width="120px" alt="github" />
           </a>
         </div>
         <p class="text-xl font-bold text-white menu-title md:absolute">
@@ -12,9 +14,11 @@
         </p>
       </div>
       <div class="relative flex flex-col items-center justify-center pb-8 overflow-hidden menu-container">
-        <div class="flex items-center justify-center w-32 h-32 transition-all duration-300 border-2 border-white rounded-full menu-button hover:border-opacity-0">
+        <div
+          class="flex items-center justify-center w-32 h-32 transition-all duration-300 border-2 border-white rounded-full menu-button hover:border-opacity-0"
+        >
           <a href="https://www.instagram.com/yusufcan.n">
-            <img :src="require('~/assets/icons/contact/instagram.svg')" width="120px" alt="instagram">
+            <img :src="require('~/assets/icons/contact/instagram.svg')" width="120px" alt="instagram" />
           </a>
         </div>
         <p class="text-xl font-bold text-white menu-title md:absolute">
@@ -22,9 +26,11 @@
         </p>
       </div>
       <div class="relative flex flex-col items-center justify-center pb-8 overflow-hidden menu-container">
-        <div class="flex items-center justify-center w-32 h-32 transition-all duration-300 border-2 border-white rounded-full menu-button hover:border-opacity-0">
+        <div
+          class="flex items-center justify-center w-32 h-32 transition-all duration-300 border-2 border-white rounded-full menu-button hover:border-opacity-0"
+        >
           <a href="mailto:info@yusufcanyilmaz.com">
-            <img :src="require('~/assets/icons/contact/mail.svg')" width="120px" alt="mail">
+            <img :src="require('~/assets/icons/contact/mail.svg')" width="120px" alt="mail" />
           </a>
         </div>
         <p class="text-xl font-bold text-white menu-title md:absolute">
@@ -32,13 +38,27 @@
         </p>
       </div>
       <div class="relative flex flex-col items-center justify-center pb-8 overflow-hidden menu-container">
-        <div class="flex items-center justify-center w-32 h-32 transition-all duration-300 border-2 border-white rounded-full menu-button hover:border-opacity-0">
+        <div
+          class="flex items-center justify-center w-32 h-32 transition-all duration-300 border-2 border-white rounded-full menu-button hover:border-opacity-0"
+        >
           <a href="https://www.linkedin.com/in/yusufcan-yilmaz/">
-            <img :src="require('~/assets/icons/contact/linkedin.svg')" width="120px" alt="linkedin">
+            <img :src="require('~/assets/icons/contact/linkedin.svg')" width="120px" alt="linkedin" />
           </a>
         </div>
         <p class="text-xl font-bold text-white menu-title md:absolute">
           Linkedin
+        </p>
+      </div>
+      <div class="relative flex flex-col items-center justify-center pb-8 overflow-hidden menu-container">
+        <div
+          class="flex items-center justify-center w-32 h-32 transition-all duration-300 border-2 border-white rounded-full menu-button hover:border-opacity-0"
+        >
+          <a href="https://yusufcan-yilmaz.medium.com/">
+            <img :src="require('~/assets/icons/contact/medium.svg')" width="120px" alt="medium" />
+          </a>
+        </div>
+        <p class="text-xl font-bold text-white menu-title md:absolute">
+          Medium
         </p>
       </div>
     </div>
@@ -56,17 +76,17 @@ export default {
 </script>
 
 <style scoped>
-.menu-button img{
+.menu-button img {
   transition: 0.3s cubic-bezier(0.075, 0.82, 0.165, 1);
 }
-.menu-button:hover img{
+.menu-button:hover img {
   transform: scale(1.4);
 }
-.menu-title{
+.menu-title {
   transition: 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);
   bottom: -18%;
 }
-.menu-button:hover + .menu-title{
+.menu-button:hover + .menu-title {
   bottom: 0%;
 }
 </style>
